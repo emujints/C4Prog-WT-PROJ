89e3bb1ef6c49884e522c02f0ec2f68e5d9f0ec0
Emujin <20126220@tafe@wa.edu.au>
2024-11-24 12:14:18 +0800

    feat(homepage): complete homepage layout and styling

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/input.css",
+      "./assets/css/site.css",
+      "./index.html",
+      "./about.html",
+      "./contact.html",
+      "./mywork.html",
+      "./projects.html",
+      "./resume.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
